5a47bba72b3d3002ebb14f407fdb025c7bd625a6
libby reeves <libbyreeves.dev@gmail.com>
2026-03-03 21:24:48 -0500

    completed first problem

---
+++ src/puzzles/Main.java
@@ -0,0 +1,10 @@
+package puzzles;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Problem1.solve();
+
+	}
+
+}
---
+++ src/puzzles/Problem1.java
@@ -0,0 +1,10 @@
+package puzzles;
+
+public class Problem1 {
+
+	public static void solve() {
+		System.out.println("Hello World");
+		
+	}
+
+}
